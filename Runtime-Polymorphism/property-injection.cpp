74dddaec899df569b1f7dc6ddce0e467f7ed3890
devereaweaver <devereweaver_dev@icloud.com>
2022-08-19 12:04:09 -0500

    injection types

--- Runtime-Polymorphism/property-injection.cpp
+++ Runtime-Polymorphism/property-injection.cpp
@@ -37,9 +37,33 @@ struct FileLogger : Logger     // second Logger implementation
 	}
 };
 
+/* Constructor injection - pass a reference to class in the constructor
+ * to create the instance of the derived class */
+
+/* This example sets the value of logger member (the derived class)
+ * in the constructor using a member initialization list 
+ * This is useful since we have to use a reference in the constructor
+ * for the derived class, this fixes the choice of logger for the duration
+ * of the Bank object. Recall, references can't be reseated, so if this is 
+ * desireable, then use this method. 
+ */
+struct Bank 
+{
+	Bank(Logger& logger) : logger{logger}{};  // Constructor injection
+	void make_transfer(long from, long to, double amount)
+	{
+		// Notionally transfer funds
+		logger.log_transfer(from, to, amount);
+	}
+	private:
+		Logger& logger;
+};
 
 int main()
 {
-	/* The actual injection methods are implemented in separate files */
+	ConsoleLogger logger;
+	Bank bank{logger};   // instance of bank with logger type in constructor
+	bank.make_transfer(1000, 2000, 49.95);
+	bank.make_transfer(2000, 4000, 20.00);
 }
 
